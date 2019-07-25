16e00344a24c062ea5714f58d8eef0e11e4ced84
Kalinina Oksana <ksan4ik92@gmail.com>
2019-07-25 21:20:36 +0300

    New code for add ul over JS in page (New and Old)

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,35 @@
 var main=function () {
 	"use.strict";
+	var massiv=["Шапки, шарфы, снуды",
+	"Свитера, платья, кардиганы",
+	"Сумки, шляпы, рюкзаки"]
 	$(".tabs a span").toArray().forEach(function(element){
 		$(element).on("click",function(){
-		$(".tabs a span").removeClass("active");
+			var el=$(element),$content;
+			$(".tabs a span").removeClass("active");
 			$(element).addClass("active");
 			$("main .content").empty();
-			console.log("hdskhjf");
+			if (el.parent().is(":nth-child(1)")){
+				$content=$("<ul>");
+				var num;
+				for (num=massiv.length-1;num!=-1;num=num-1){
+					$content.append($("<li>").text(massiv[num]))
+					$("main .content").append($content);
+				}	
+			}
+			else if (el.parent().is(":nth-child(2)")){
+				$content=$("<ul>");
+				massiv.forEach(function(todo){
+					$content.append($("<li>").text(todo))
+					$("main .content").append($content);
+				});
+			}
+			else{
+				console.log("three");
+			}
 			return false;
 		})
+		$(".tabs a:first-child span").trigger("click");
 	});
 };
 $(document).ready(main);
