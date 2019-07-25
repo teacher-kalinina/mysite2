8b14cea23475875e75f20d9a08afcac39149cce6
Kalinina Oksana <ksan4ik92@gmail.com>
2019-07-25 19:29:05 +0300

    use forEach

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,23 +1,13 @@
-var act=function(n){
-	$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(n) span").addClass("active");
-		$("main .content").empty();
-		return false;
-}
 var main=function () {
-	"use.stict";
-	$(".tabs a:nth-child(1)").on("click",function(){
-		var n=1;
-		act(1);
+	"use.strict";
+	$(".tabs a span").toArray().forEach(function(element){
+		$(element).on("click",function(){
+		$(".tabs a span").removeClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
+			console.log("hdskhjf");
+			return false;
+		})
 	});
-	$(".tabs a:nth-child(2)").on("click",function(){
-		var n=2;
-		act(2);
-	});
-	$(".tabs a:nth-child(3)").on("click",function(){
-		var n=3;
-		act(3);
-	});
-
 };
 $(document).ready(main);
